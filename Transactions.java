56d3186be1ac4a70a10a4aa9abb6e259fa8d0309
bastienwcs <bastien@wildcodeschool.fr>
2019-04-26 15:13:03 +0200

    fix time

--- Transactions.java
+++ Transactions.java
@@ -4,7 +4,7 @@ import java.util.TimerTask;
 public class Transactions {
 
     public static void withdraw(Bank account, int value) {
-        int time = (int) (Math.random() * ((4000 - 1000) + 1)) + 1000;
+        int time = (int) (Math.random() * ((1000 - 500) + 1)) + 500;
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
